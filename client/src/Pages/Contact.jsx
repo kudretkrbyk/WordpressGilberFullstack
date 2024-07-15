999f0fffab2420504378eba33985605cac553b00
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-15 18:16:29 +0300

    frontend getcontact added

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,18 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default function Contact() {
+  const [contactData, setContactData] = useState([]);
+
+  useEffect(() => {
+    // Verileri getir
+    axios
+      .get("http://localhost:3000/contact")
+      .then((response) => {
+        setContactData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching contact:", error);
+      });
+  }, []);
+
   return (
     <div className="w-full xl:h-screen h-[1000px] md:h-[850px]  relative">
       <div className="z-30 absolute bg-cover bg-center w-full h-full bg-[url(http://paul-themes.com/wordpress/gilber/wp-content/uploads/2020/12/contact.jpg)]   "></div>
       <div className="absolute z-40 flex flex-col xl:flex-row items-center justify-center gap-8 xl:gap-20 w-full h-full text-white p-10 md:p-40">
-        <div className="z-40 flex flex-col gap-5 items-start justify-around  w-full h-full  ">
-          <div className="text-5xl">Contact</div>
-          <div className=" text-zinc-400 w-full ">
-            You’ll called htmlFor yielding male, so lights Stars abundantly, is
-            their.
+        {contactData.map((data) => (
+          <div
+            key={data.id}
+            className="z-40 flex flex-col gap-5 items-start justify-around  w-full h-full  "
+          >
+            <div className="text-5xl">Contact</div>
+            <div className=" text-zinc-400 w-full ">{data.contact_title}</div>
+            <div className="text-4xl">{data.contact_address} </div>
+            <div className="text-3xl">{data.contact_phone_number} </div>
+            <div className=" text-zinc-400 ">{data.contact_mail} </div>
           </div>
-          <div className="text-4xl">27 Queen St, New York USA</div>
-          <div className="text-3xl">+7 (212) 674-25-10</div>
-          <div className=" text-zinc-400 ">gilber.design@gmail.com</div>
-        </div>
+        ))}
+
         <div className=" w-full h-full text-white ">
           <form className="flex flex-col gap-5">
             <div className="flex gap-3">
--- server/api/get/getContact.js
+++ server/api/get/getContact.js
@@ -3,7 +3,7 @@ const pool = require("../../config/db");
 
 const getContact = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM home");
+    const result = await pool.query("SELECT * FROM contact");
     res.json(result.rows);
   } catch (err) {
     console.error("Error executing query:", err);
