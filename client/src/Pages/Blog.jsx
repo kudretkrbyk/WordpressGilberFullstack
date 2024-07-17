f0b1786ba6f4beaf95a37a786e68e2727fd5191a
kudretkrbyk <kudretkrbyk@gmail.com>
2024-07-17 10:41:49 +0300

    frontend blogPage added edit button

--- client/src/Pages/Blog.jsx
+++ client/src/Pages/Blog.jsx
@@ -1,9 +1,13 @@
 import AnimationBorder from "../Components/AnimationBorder";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+
+import { MdEditNote } from "react-icons/md";
 
 export default function Blog() {
   const [recentNewsData, setRecentNewsData] = useState([]);
+  const activeComponent = useSelector((state) => state.activeComponent);
 
   useEffect(() => {
     // Verileri getir
@@ -21,29 +25,62 @@ export default function Blog() {
     <div className=" w-full xl:h-screen flex items-center justify-center text-white bg-[#161616] ">
       <div className=" flex flex-col gap-10 w-full p-5 md:p-32 xl:p-48  items-start justify-center">
         <div className="text-xl">Recent news</div>
-        <div className="flex flex-col xl:flex-row w-full h-full gap-10">
-          {recentNewsData.map((data) => (
-            <div key={data.id} className="group w-full relative">
-              <div className=" w-full">
-                <AnimationBorder></AnimationBorder>
-              </div>
-              <div
-                className={`bg-cover w-full h-[400px] bg-center bg-[url(${data.recent_photo})]`}
-              ></div>
-              <div className=" top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 duration-500 ">
-                <div className=" top-0 left-0   w-full h-[400px] absolute bg-[#161616] p-5 px-10 flex flex-col gap-14">
-                  <div>{data.recent_date} </div>
-                  <div className="text-2xl font-bold">{data.recent_title}</div>
-                  <div className="flex flex-col gap-20">
-                    {" "}
-                    <div>{data.recent_comment}</div>
-                    <div>Read More</div>
+        {activeComponent === "Blog" ? (
+          <div className="relative flex flex-col xl:flex-row w-full h-full gap-10 border border-white p-10">
+            <div className="absolute z-40 top-0 right-0">
+              <MdEditNote className="text-white size-10" />
+            </div>
+            {recentNewsData.map((data) => (
+              <div key={data.id} className="group w-full relative">
+                <div className=" w-full">
+                  <AnimationBorder></AnimationBorder>
+                </div>
+                <div
+                  className={`bg-cover w-full h-[400px] bg-center bg-[url(${data.recent_photo})]`}
+                ></div>
+                <div className=" top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 duration-500 ">
+                  <div className=" top-0 left-0   w-full h-[400px] absolute bg-[#161616] p-5 px-10 flex flex-col gap-14">
+                    <div>{data.recent_date} </div>
+                    <div className="text-2xl font-bold">
+                      {data.recent_title}
+                    </div>
+                    <div className="flex flex-col gap-20">
+                      {" "}
+                      <div>{data.recent_comment}</div>
+                      <div>Read More</div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col xl:flex-row w-full h-full gap-10 ">
+            {recentNewsData.map((data) => (
+              <div key={data.id} className="group w-full relative">
+                <div className=" w-full">
+                  <AnimationBorder></AnimationBorder>
+                </div>
+                <div
+                  className={`bg-cover w-full h-[400px] bg-center bg-[url(${data.recent_photo})]`}
+                ></div>
+                <div className=" top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 duration-500 ">
+                  <div className=" top-0 left-0   w-full h-[400px] absolute bg-[#161616] p-5 px-10 flex flex-col gap-14">
+                    <div>{data.recent_date} </div>
+                    <div className="text-2xl font-bold">
+                      {data.recent_title}
+                    </div>
+                    <div className="flex flex-col gap-20">
+                      {" "}
+                      <div>{data.recent_comment}</div>
+                      <div>Read More</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
